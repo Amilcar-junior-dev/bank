8182d80b16aa769e92e83b81ab1a6e95481f8dad
Amilcar-junior-dev <101715730+Amilcar-junior-dev@users.noreply.github.com>
2022-04-19 22:40:17 -0300

    implementation components

---
+++ src/Components/TabBar/data.tsx
@@ -0,0 +1,25 @@
+
+export const data = [
+    {
+        id: 0,
+        name: 'Home',
+        screenKey: 'HomeInteligence',
+        default: true,
+        icon: 'home'
+    },
+    {
+        id: 1,
+        name: 'Transferencia',
+        screenKey: 'TransferInteligence',
+        default: false,
+        icon: 'credit-card'
+    },
+    {
+        id: 2,
+        name: 'Profile',
+        screenKey: 'ProfileInteligence',
+        default: false,
+        icon: 'user'
+    },
+]
+
